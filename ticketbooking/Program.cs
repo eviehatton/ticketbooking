04a231ff38388e09ebf5d620da526297cfff225b
eviehatton <eviehattonxo@gmail.com>
2021-03-07 15:47:16 +0000

    creating menu

--- ticketbooking/Program.cs
+++ ticketbooking/Program.cs
@@ -6,7 +6,44 @@ namespace ticketbooking
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            static void Menu()
+            {
+                Console.WriteLine("Ticket Booking");
+                Console.WriteLine("-----------------------------------");
+                Console.WriteLine("1. Book tickets");
+                Console.WriteLine("2. View Events");
+                Console.WriteLine("3. Login into account");
+                Console.WriteLine("4. Admin Login");
+                int MenuChoice = int.Parse(Console.ReadLine());
+
+                if (MenuChoice == 1)
+                {
+
+                }
+
+                if (MenuChoice == 2)
+                {
+
+                }
+
+                if (MenuChoice == 3)
+                {
+
+                }
+
+                if (MenuChoice == 4)
+                {
+
+                }
+                else
+                {
+                    Console.WriteLine("not a valid input");
+                    Menu();
+                }
+
+            }
+
+
         }
     }
 }
---
+++ ticketbooking/ViewEvents.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ticketbooking
+{
+    class ViewEvents
+    {
+
+    }
+}
